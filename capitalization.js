9becfbf53f9f5c7b74867337c8e955c0ac48bd56
Alexandra Maracinaru <alexandramaracinaru@gmail.com>
2021-02-08 23:05:00 +0100

    to lower case added

--- capitalization.js
+++ capitalization.js
@@ -1,17 +1,17 @@
-const fullName = "alexandra maracinaru";
-const uppedOne = fullName.substring(0,1).toUpperCase();
+const fullName = "alexanKJra maracOIUaru";
+const firstLetter = fullName.substring(0,1).toUpperCase();
 const firstSpace = fullName.indexOf(' ');
 
-const firstName = fullName.substring(1, firstSpace);
+const firstName = fullName.substring(1, firstSpace).toLowerCase();
 const lastSpace = fullName.lastIndexOf(' ');
-const lastName = fullName.substring(lastSpace +2);
-const uppedTwo = fullName.substring(firstSpace +1, firstSpace +2).toUpperCase();
-//console.log(uppedOne)
-//console.log(firstName)
-//console.log(uppedTwo)
-//console.log(lastName)
+const lastName = fullName.substring(lastSpace +2).toLowerCase();
+const firstLetterLastName = fullName.substring(firstSpace +1, firstSpace +2).toUpperCase();
+console.log(firstLetter)
+console.log(firstName)
+console.log(firstLetterLastName)
+console.log(lastName)
 
-const allTogether = uppedOne + firstName + " " + uppedTwo + lastName;
+const allTogether = firstLetter + firstName + " " + firstLetterLastName + lastName;
 console.log(allTogether);
 
 
