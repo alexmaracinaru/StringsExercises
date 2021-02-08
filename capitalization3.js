cec8927d0843fd4d99390c3c4e8b160cf8ccad6d
Alexandra Maracinaru <alexandramaracinaru@gmail.com>
2021-02-08 17:50:21 +0100

    capitalization3

---
+++ capitalization3.js
@@ -0,0 +1,16 @@
+const primaryName = "peter";
+const thirdUpperCase = primaryName.substring(2,3).toUpperCase();
+const spelledWeirdly = primaryName.substring(0,2) + thirdUpperCase + primaryName.substring(3);
+console.log(spelledWeirdly);
+
+
+
+const upName = "PETER";
+const fourthDown = upName.substring(3,4).toLowerCase();
+const weirdAgain = upName.substring(0,3) + fourthDown + upName.substring(4);
+console.log(weirdAgain);
+
+const myName = "alexandra"
+const allCaps = myName.substring(0).toUpperCase();
+
+
